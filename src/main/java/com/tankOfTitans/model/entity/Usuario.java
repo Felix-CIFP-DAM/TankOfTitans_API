293f1bb5c773cfe5193854fed0fe6eab57890c0a
Daniel Cuervo Álvarez <daniel.cuervo@informatica.cifpaviles.pa>
2026-04-22 21:34:30 +0200

    añadidos

--- src/main/java/com/tankOfTitans/model/entity/Usuario.java
+++ src/main/java/com/tankOfTitans/model/entity/Usuario.java
@@ -37,6 +37,7 @@ public class Usuario {
     @Column(unique = true, nullable = false, length = 100)
     private String email;
 	
+	
 	@Column(name = "created_at")
 	   private LocalDateTime createdAt;
 
---
+++ src/main/java/com/tankOfTitans/model/entity/enums/Rol.java
@@ -0,0 +1,6 @@
+package com.tankOfTitans.model.entity.enums;
+
+public enum Rol {
+	USER,
+	SUPERUSER
+}
